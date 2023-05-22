74f3df9ba4d7362a0695189060c28ac56b0ba4a8
Quinn Booth <qabooth@gmail.com>
2023-05-22 05:23:51 -0400

    Color picking and rotation reorientation
    Made every other circle rotate in the opposite direction and implemented a color picker.

--- fractals/spirograph/spirograph.js
+++ fractals/spirograph/spirograph.js
@@ -9,6 +9,7 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d", { willReadFrequently: true });
 const fractal_canvas = document.getElementById("fractal_canvas");
 const fractal_ctx = fractal_canvas.getContext("2d", { willReadFrequently: true });
+const description = document.getElementById("description");
 
 // Clear both canvases
 function reset_canvases() {
@@ -18,35 +19,72 @@ function reset_canvases() {
     fractal_ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-// Ensure canvas is square and fits on screen
-window.addEventListener('resize', function() {
-    console.log("test");
-    if (window.innerWidth > window.innerHeight) {
-        canvas.height = Math.floor(window.innerHeight * 0.75);
+// Clear just the circles
+function clear_circles() {
+    ctx.fillStyle = "#0E0B16";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+// Make everything fit on screen
+function fit() {
+    if (window.innerWidth * 0.65 > window.innerHeight * 0.8) {
+        canvas.height = Math.floor(window.innerHeight * 0.8 * 0.75);
         canvas.width = canvas.height;
         fractal_canvas.width = canvas.height;
         fractal_canvas.height = canvas.height;
     } else {
-        canvas.width = Math.floor(window.innerWidth * 0.75);
+        canvas.width = Math.floor(window.innerWidth * 0.65 * 0.75);
         canvas.height = canvas.width;
         fractal_canvas.width = canvas.width;
         fractal_canvas.height = canvas.width;
     }
-    
+    if (window.innerWidth <= 915) {
+        canvas.style.left = "40%";
+        fractal_canvas.style.left = "40%";
+    } else {
+        canvas.style.left = "";
+        fractal_canvas.style.left = "";
+    }
+    if (window.innerHeight <= 520) {
+        description.style.height = "120px";
+    } else {
+        description.style.height = "275px";
+    }
     reset_canvases();
-});
+}
 
-window.dispatchEvent(new Event('resize'));
+// Ensure everything fits on screen when resized
+window.addEventListener('resize', function() {
+    fit();
+});
 
 //#endregion
 
-var num_of_circles = 4;
-var current_circle = null;
+// Stores an object for each circle
 var circles = [];
-var radius = canvas.width / 3;
-var color = "#FFFFFF";
-var step = 1000;
 
+// RGB of fractal
+var colors = [255, 0, 0];
+var color_tracker = [1, -1, -1];
+var rainbow = false;
+
+// Color options
+function set_red() {
+    colors = [255, 0, 0];
+    rainbow = false;
+}
+
+function set_white() {
+    colors = [255, 255, 255];
+    rainbow = false;
+}
+
+function set_multi() {
+    colors = [255, 50, 0];
+    rainbow = true;
+}
+
+// Controls the rotation and placement of each ccircle
 class Circle {
 
     constructor(parent, radius, color, step) {
@@ -55,12 +93,15 @@ class Circle {
         this.color = color;
         this.step = 2 * Math.PI * (1 / step);  // How many steps to make a full revolution around circle
         this.cumulative_step = this.step;  // Step of this circle + all its parents
+        this.direction = 1;
         this.x = canvas.width / 2;
         this.y = canvas.height / 2;
         if (this.parent) {
-            this.x = parent.x_connect - radius;
+            // this.x = parent.x_connect - radius;
+            this.x = parent.x_connect + radius;
             this.y = parent.y_connect;
             this.cumulative_step = this.step + this.parent.cumulative_step;
+            this.direction = this.parent.direction * -1;  // Have child rotate in opposite direction as parent
         }
         this.angle = 0;
         this.x_connect = this.x + this.radius;
@@ -69,8 +110,8 @@ class Circle {
 
     get_coords() {
         if (this.parent) {
-            this.x = this.parent.x_connect - this.radius * Math.cos(-1 * this.parent.angle);
-            this.y = this.parent.y_connect + this.radius * Math.sin(-1 * this.parent.angle);
+            this.x = this.parent.x_connect + this.radius * Math.cos(-1 * this.parent.angle);
+            this.y = this.parent.y_connect - this.radius * Math.sin(-1 * this.parent.angle);
         }
     }
 
@@ -85,39 +126,90 @@ class Circle {
     trace() {
         this.x_connect = this.x + this.radius * Math.cos(this.angle);
         this.y_connect = this.y + this.radius * Math.sin(this.angle);
-        this.angle += this.cumulative_step;
+        this.angle += this.cumulative_step * this.direction;
     }
 
 }
 
-function delayed_loop(iterations, delay, func) {
+// Runs a number of steps of the fractal algorithm
+function draw_fractal(iterations, delay) {
     for (let i = 0; i < iterations; i++) {
-        (function (i) {
-            setTimeout(function (){
-                func();
-            }, delay * i);
-        })(i);
+      (function (i) {
+        setTimeout(function () {
+          move_circles(i, iterations, function () {
+            if (i === iterations - 1) {
+                setTimeout(clear_circles, 10);
+            }
+          });
+        }, delay * i);
+      })(i);
     }
 }
 
-function draw_fractal() {
+function change_color() {
+    if (!rainbow) return;
+    for (let i = 0; i < colors.length; i++) {
+        colors[i] = colors[i] + color_tracker[i];
+        if (colors[i] > 255) {
+            colors[i] = 255;
+            color_tracker[i] = -1;
+        } else if (colors[i] < 0) {
+            colors[i] = 0;
+            color_tracker[i] = 1;
+        }
+    }
+}
+  
+// Moves each circle by one step in their rotation and plots the fractal
+function move_circles(i, iterations, callback) {
     ctx.fillStyle = "#0E0B16";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < circles.length; i++) {
-        circles[i].get_coords();
-        circles[i].draw();
-        circles[i].trace();
+      circles[i].get_coords();
+      circles[i].draw();
+      circles[i].trace();
     }
-    fractal_ctx.fillStyle = "#FF0000";
+    change_color();
+    fractal_ctx.fillStyle = `rgb(${colors[0]}, ${colors[1]}, ${colors[2]})`;
     const last_circle = circles.length - 1;
     fractal_ctx.fillRect(circles[last_circle].x_connect, circles[last_circle].y_connect, 1, 1);
+    setTimeout(callback, 10);
 }
 
-for (let i = 0; i < num_of_circles; i++) {
-    let circle = new Circle(current_circle, radius, color, step);
-    circles.push(circle);
-    radius = radius / 2;
-    current_circle = circle;
+// Initializes the circle objects
+function create_circles(num_of_circles, steps) {
+
+    let current_circle = null;
+    let radius = canvas.width / 6;
+    let color = "#FFFFFF";
+
+    for (let i = 0; i < num_of_circles; i++) {
+        let circle = new Circle(current_circle, radius, color, steps[i]);
+        circles.push(circle);
+        radius = radius / 2;
+        current_circle = circle;
+    }
+
+    move_circles();
+
 }
 
-delayed_loop(10000, 0.1, draw_fractal);
+//let steps = [500, 20000, 500, 500, 500];
+//let steps = [500, 1000, 20000];
+//let steps = [16000, 100, 100];
+//let steps = [100000, 1000, 100];
+
+
+//let steps = [100000, 1600, 800, 400, 200, 100, 10];
+//let steps = [40000, 10000, 10000, 1000, 500, 100, 10];
+//let steps = [40000, 20000, 20000, 1000, 500, 100, 10];
+
+
+// Resize elements and draw initial fractal on launch
+fit();
+let steps = [80000, 20000, 10000, 5000, 1000, 500, 100, 10];
+setTimeout(function() {
+    create_circles(steps.length, steps);
+    draw_fractal(Math.max.apply(null, steps), 0.1);
+}, 10);
+
