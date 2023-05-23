288c8d886edfd76debe52d5b2790606f9f2057b7
Quinn Booth <qabooth@gmail.com>
2023-05-23 02:56:07 -0400

    Fractal skeletons
    Created skeletons for the 4 remaining fractals.

---
+++ fractals/julia/julia.js
@@ -0,0 +1,41 @@
+//
+// Julia Set Visualization
+// Author: Quinn Booth
+//
+
+// HTML elements
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d", { willReadFrequently: true });
+const back_button = document.getElementById("back_button");
+const description = document.getElementById("description");
+
+// Enable re-direct to home page
+back_button.addEventListener("click", function() {
+    window.location.href = "../../index.html";
+});
+
+// Ensure everything fits on screen when resized
+window.addEventListener('resize', fit);
+
+// Clear the canvas
+function reset_canvas() {
+    ctx.fillStyle = "#0E0B16";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+// Make everything fit on screen
+function fit() {
+    if (window.innerWidth * 0.65 > window.innerHeight * 0.8) {
+        canvas.height = Math.floor(window.innerHeight * 0.8 * 0.75);
+        canvas.width = canvas.height;
+    } else {
+        canvas.width = Math.floor(window.innerWidth * 0.65 * 0.75);
+        canvas.height = canvas.width;
+    }
+    canvas.style.left = (window.innerWidth <= 915) ? "40%" : "";
+    description.style.height = (window.innerHeight <= 520) ? "120px" : "275px";
+    reset_canvas();
+}
+
+// Resize everything upon launch and plot initial fractal
+fit();
--- main.js
+++ main.js
@@ -6,3 +6,19 @@
 function goto_spirograph() {
     window.location.href = "./fractals/spirograph/spirograph.html";
 }
+
+function goto_fern() {
+    window.location.href = "./fractals/fern/fern.html";
+}
+
+function goto_mandelbrot() {
+    window.location.href = "./fractals/mandelbrot/mandelbrot.html";
+}
+
+function goto_julia() {
+    window.location.href = "./fractals/julia/julia.html";
+}
+
+function goto_tree() {
+    window.location.href = "./fractals/tree/tree.html";
+}
