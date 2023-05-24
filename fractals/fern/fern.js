f66c1f9323ce70bfad57c0639f5d0c3c02ff49b3
Quinn Booth <qabooth@gmail.com>
2023-05-24 03:58:37 -0400

    Finalized Barnsley Fern Interaction
    Interactable matrix added for manual manipulation of coefficients/summands/probabilities in the algorithm. Scale slider.

--- fractals/fern/fern.js
+++ fractals/fern/fern.js
@@ -3,14 +3,14 @@
 // Author: Quinn Booth
 //
 
-//#region SKELETON
-
 // HTML elements
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d", { willReadFrequently: true });
 const back_button = document.getElementById("back_button");
 const description = document.getElementById("description");
 const color_picker = document.getElementById('color_picker');
+const matrix_cells = document.getElementsByClassName("matrix_cell");
+const slider = document.getElementById("slider");
 
 // Enable re-direct to home page
 back_button.addEventListener("click", function() {
@@ -18,7 +18,10 @@ back_button.addEventListener("click", function() {
 });
 
 // Ensure everything fits on screen when resized
-window.addEventListener('resize', fit);
+window.addEventListener('resize', function() {
+    drawing_no++;
+    fit();
+});
 
 // Clear the canvas
 function reset_canvas() {
@@ -26,17 +29,25 @@ function reset_canvas() {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+// User initiated clear
+function user_clear() {
+    drawing_no++;
+    ctx.fillStyle = "#0E0B16";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
 // Make everything fit on screen
 function fit() {
-    if (window.innerWidth * 0.65 > window.innerHeight * 0.8) {
-        canvas.height = Math.floor(window.innerHeight * 0.8 * 0.75);
+    if (window.innerWidth * 0.65 > window.innerHeight * 0.75) {
+        canvas.height = Math.floor(window.innerHeight * 0.75 * 0.75);
         canvas.width = canvas.height;
     } else {
         canvas.width = Math.floor(window.innerWidth * 0.65 * 0.75);
         canvas.height = canvas.width;
     }
     canvas.style.left = (window.innerWidth <= 915) ? "40%" : "";
-    description.style.height = (window.innerHeight <= 520) ? "120px" : "275px";
+    description.style.height = (window.innerHeight <= 700) ? "120px" : "350px";
+    user_scale = canvas.height * parseInt(slider.value) / 50;
     reset_canvas();
 }
 
@@ -45,19 +56,62 @@ color_picker.addEventListener('change', function(event) {
     user_color = event.target.value;
 });
 
+// Allow user to pick a scale for their fractals
+slider.addEventListener("input", function() {
+    user_scale = canvas.height * parseInt(slider.value) / 50;
+});
+
+
 // Let user plot ferns by clicking on canvas
 canvas.addEventListener('click', function(event) {
     const canvas_bounds = canvas.getBoundingClientRect();
     const canvas_x = event.clientX - canvas_bounds.left;
     const canvas_y = event.clientY - canvas_bounds.top;
-    //barnsley(event.clientX - canvas_bounds.left, event.clientY - canvas_bounds.top, canvas.height * 0.08, user_color, 10000, 0.1);
-    fern(user_coefficient, user_summand, user_probability, iterations, canvas_x, canvas_y, user_scale, user_color, user_speed);
+    let adjusted_scale = user_scale;
+    if (user_preset == "cyclosorus") {
+        adjusted_scale = adjusted_scale * 1.5;
+    } else if (user_preset == "tree") {
+        adjusted_scale = adjusted_scale * 20;
+    }
+    fern(user_coefficient, user_summand, user_probability, iterations, canvas_x, canvas_y, adjusted_scale, user_color, 0.2);
 });
 
-// Resize everything upon launch and plot initial fractal
-fit();
+// Match the fields in our onscreen matrix to our user values
+function match_matrix() {
 
-//#endregion
+    for (let i = 0; i < 4; i++) {
+        matrix_cells[i*7].value = user_coefficient[i][0][0];
+        matrix_cells[i*7 + 1].value = user_coefficient[i][0][1];
+        matrix_cells[i*7 + 2].value = user_coefficient[i][1][0];
+        matrix_cells[i*7 + 3].value = user_coefficient[i][1][1];
+        matrix_cells[i*7 + 4].value = user_summand[i][0][0];
+        matrix_cells[i*7 + 5].value = user_summand[i][1][0];
+        matrix_cells[i*7 + 6].value = user_probability[i];
+    }
+
+}
+
+// Match user values to those in onscreen matrix
+function match_vals() {
+
+    for (let i = 0; i < 4; i++) {
+        user_coefficient[i][0][0] = parseFloat(matrix_cells[i*7].value);
+        user_coefficient[i][0][1] = parseFloat(matrix_cells[i*7 + 1].value);
+        user_coefficient[i][1][0] = parseFloat(matrix_cells[i*7 + 2].value);
+        user_coefficient[i][1][1] = parseFloat(matrix_cells[i*7 + 3].value);
+        user_summand[i][0][0] = parseFloat(matrix_cells[i*7 + 4].value);
+        user_summand[i][1][0] = parseFloat(matrix_cells[i*7 + 5].value);
+        user_probability[i] = parseFloat(matrix_cells[i*7 + 6].value);
+    }
+
+}
+
+// Have it so changing any value in the onscreen matrix triggers the above function
+for (let i = 0; i < matrix_cells.length; i++) {
+    matrix_cells[i].addEventListener('input', function() {
+        match_vals();
+    });
+}
 
 // Coefficients, summands, and probabilities used to generate Barnsley Fern and other variants.
 // Kept as dictionaries for legibility.
@@ -125,15 +179,15 @@ const tree_probability = {
     3: 0.15
 };
 
-
 // User variables
 const iterations = 50000;  // 50000 is a good sweet spot
-var user_coefficient = Object.assign({}, barnsley_coefficient);
-var user_summand = Object.assign({}, barnsley_summand);
+var user_coefficient = custom_copy(barnsley_coefficient);
+var user_summand = custom_copy(barnsley_summand);
 var user_probability = Object.assign({}, barnsley_probability);
-var user_scale = canvas.height * 0.08;
-var user_speed = 0.1;
+var user_scale = 0;
+var user_preset = "barnsley";
 var user_color = "green";
+var drawing_no = 0;
 
 // Create a point class so we can easily draw multiple ferns
 
@@ -203,6 +257,22 @@ function multiply_matrix(m1, m2) {
     return product;
 }
 
+// Function for copying coefficient/summand structure
+
+function custom_copy(d) {
+    let dict = {};
+    for (let i in d) {
+        dict[i] = [];
+        for (let j = 0; j < d[i].length; j++) {
+            dict[i].push([]);
+            for (let k = 0; k < d[i][j].length; k++) {
+                dict[i][j].push(d[i][j][k]);
+            }
+        }
+    }
+    return dict;
+}
+
 // Implement a function that executes the Barnsley Fern algorithm and draws on the canvas
 
 function fern(coefficients, summands, probabilities, iterations, x, y, scale, color, delay) {
@@ -217,9 +287,11 @@ function fern(coefficients, summands, probabilities, iterations, x, y, scale, co
 
     for (let n = 0; n < iterations; n++) {
 
-        (function (n) {
+        (function (num, n) {
 
             setTimeout(function() {
+
+                if (num != drawing_no) return;
 
                 ctx.fillStyle = color;
                 const p = Math.random();
@@ -240,7 +312,7 @@ function fern(coefficients, summands, probabilities, iterations, x, y, scale, co
 
             }, delay * n);
 
-        })(n);
+        })(drawing_no, n);
 
     }
 }
@@ -259,27 +331,38 @@ function tree(x, y, scale, color, iterations, delay) {
     fern(tree_coefficient, tree_summand, tree_probability, iterations, x, y, scale, color, delay);
 }
 
+// Functions to facilitate user selecting presets in drop down menu
+
 function select_barnsley() {
-    user_coefficient = Object.assign({}, barnsley_coefficient);
-    user_summand = Object.assign({}, barnsley_summand);
+    user_coefficient = custom_copy(barnsley_coefficient);
+    user_summand = custom_copy(barnsley_summand);
     user_probability = Object.assign({}, barnsley_probability);
+    user_preset = "barnsley";
+    match_matrix();
 }
 
 function select_cyclosorus() {
-    user_coefficient = Object.assign({}, cyclosorus_coefficient);
-    user_summand = Object.assign({}, cyclosorus_summand);
+    user_coefficient = custom_copy(cyclosorus_coefficient);
+    user_summand = custom_copy(cyclosorus_summand);
     user_probability = Object.assign({}, cyclosorus_probability);
+    user_preset = "cyclosorus";
+    match_matrix();
 }
 
 function select_tree() {
-    user_coefficient = Object.assign({}, tree_coefficient);
-    user_summand = Object.assign({}, tree_summand);
+    user_coefficient = custom_copy(tree_coefficient);
+    user_summand = custom_copy(tree_summand);
     user_probability = Object.assign({}, tree_probability);
+    user_preset = "tree";
+    match_matrix();
 }
 
 // Draw nice two-toned Barnsley Fern on boot
 
+fit();
 reset_canvas();
+match_matrix();
+user_scale = canvas.height * parseInt(slider.value) / 50;
 barnsley(canvas.width * 0.4, canvas.height * 0.9, canvas.height * 0.08, "blue", 10000, 0.1);
 barnsley(canvas.width * 0.4, canvas.height * 0.9, canvas.height * 0.08, "green", 70000, 0.1);
 
