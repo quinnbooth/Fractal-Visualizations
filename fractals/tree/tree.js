50614e3381385845ee3b559ca752d5d12906ff39
Quinn Booth <qabooth@gmail.com>
2023-05-29 21:36:49 -0400

    L-System Algorithm
    Implemented L-System algorithm and plotting function. 3 presets: fractal tree, Sierpinski triangle, and dragon curve.

--- fractals/tree/tree.js
+++ fractals/tree/tree.js
@@ -3,6 +3,147 @@
 // Author: Quinn Booth
 //
 
+// Pre-defined rules and alphabet for L-System Tree
+// NOTE: +, -, [, and ] are reserved for specific operations listed below
+
+// Fractal Tree
+const tree_axiom = "F";
+const tree_rules = {
+    "F": "FF+[+F-F-F]-[-F+F+F]",
+};
+
+// Sierpinski Triangle
+const sierpinski_axiom = "F-G-G";
+const sierpinski_rules = {
+    "F": "F-G+F+G-F",
+    "G": "GG"
+};
+
+// Dragon Curve
+const dragon_axiom = "F";
+const dragon_rules = {
+    "F": "F+G",
+    "G": "F-G"
+};
+
+// Plot an L-System with given specifications
+function draw_system(type, steps, angle_step) {
+
+    let axiom = tree_axiom;
+    let rules = tree_rules;
+
+    if (type == "triangle") {
+        axiom = sierpinski_axiom;
+        rules = sierpinski_rules;
+    } else if (type == "dragon") {
+        axiom = dragon_axiom;
+        rules = dragon_rules;
+    }
+
+    const string = l_system(axiom, rules, steps);
+
+    let saved_positions = [];
+    let angle = -Math.PI / 2;
+    let length = canvas.height / 65;
+
+    let x = canvas.width / 2;
+    let y = canvas.height - 25;
+
+    ctx.strokeStyle = 'lime';
+
+    // Judge how big the drawing should be based on its complexity and type
+    if (steps == 5) {
+        length = length / 2;
+        ctx.lineWidth = 0.25;
+    } else if (steps >= 6) {
+        length = length / 4;
+        ctx.lineWidth = 0.25;
+    } else {
+        ctx.lineWidth = 2;
+    }
+
+    // Center drawing
+    ctx.moveTo(x, y);
+
+    // Go symbol by symbol to determine what to draw and where
+    for (let i = 0; i < string.length; i++) {
+
+        const symbol = string[i];
+        
+        // + means rotate clockwise
+        if (symbol == "+") {
+
+            angle += angle_step;
+
+        // - means rotate counterclockwise
+        } else if (symbol == "-") {
+
+            angle -= angle_step;
+
+        // [ means save position
+        } else if (symbol == "[") {
+
+            saved_positions.push([x, y, angle]);
+
+        // ] means return to last saved position
+        } else if (symbol == "]") {
+
+            const return_to = saved_positions.pop();
+            x = return_to[0];
+            y = return_to[1];
+            angle = return_to[2];
+
+        // Otherwise, draw a line in the direction of the current angle
+        } else {
+            const new_x = x + length * Math.cos(angle);
+            const new_y = y + length * Math.sin(angle);
+            ctx.beginPath();
+            ctx.moveTo(x, y)
+            ctx.lineTo(new_x, new_y);
+            ctx.stroke();
+            x = new_x;
+            y = new_y;
+        }
+
+    }
+
+}
+
+// Iteratively goes through string, replacing characters based on rules
+function l_system(axiom, rules, steps) {
+
+    let string = axiom;
+
+    for (let i = 0; i < steps; i++) {
+        string = step(string, rules);
+    }
+
+    return string;
+
+}
+
+// Go through string and replace characters based on rules (once)
+function step(string, rules) {
+
+    let next_string = "";
+
+    for (let i = 0; i < string.length; i++) {
+        const key = string[i];
+        if (key in rules) {
+            next_string += rules[key];
+        } else {
+            next_string += string[i];
+        }
+    }
+
+    return next_string;
+}
+
+// Determine sizing of drawing based on steps and type
+function get_size(type, steps) {
+
+}
+
 // HTML elements
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d", { willReadFrequently: true });
@@ -39,3 +180,14 @@ function fit() {
 
 // Resize everything upon launch and plot initial fractal
 fit();
+
+// Draw initial L-System Tree
+// draw_system(axiom, rules, 5, -0.45);  // For Fractal Tree
+
+//const degrees = 120;
+//const radians = degrees * Math.PI / 180;
+//draw_system(axiom, rules, 7, radians);  // For Sierpinski Triangle
+
+const degrees = 90;
+const radians = degrees * Math.PI / 180;
+draw_system("tree", 5, -0.45);
